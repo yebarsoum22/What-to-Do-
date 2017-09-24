439a598b8c24505cc730d16ef685175202b3727a
yebarsoum22 <yebarsoum22@whitnall.com>
2017-09-24 18:29:56 -0500

    Initial Commit

---
+++ What to Do?Tests/What_to_Do_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  What_to_Do_Tests.swift
+//  What to Do?Tests
+//
+//  Created by Youssef Barsoum on 9/24/17.
+//  Copyright © 2017 Youssef Barsoum. All rights reserved.
+//
+
+import XCTest
+@testable import What_to_Do_
+
+class What_to_Do_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
